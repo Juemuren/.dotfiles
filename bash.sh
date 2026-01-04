0e962d4e826ba91f46e4f9c181c1d3a18c405dec
Raind <127611616+Juemuren@users.noreply.github.com>
2026-01-04 18:43:11 +0800

    bash: add dotfiles

---
+++ bash.sh
@@ -0,0 +1,32 @@
+# If not running interactively, don't do anything
+[[ "$-" != *i* ]] && return
+
+# # Write the following line near the top of .bashrc
+# [[ $- == *i* ]] && source ~/blesh/ble.sh --noattach
+
+# Integration
+eval "$(fzf --bash)"
+eval "$(starship init bash)"
+eval "$(zoxide init bash)"
+
+# https://code.visualstudio.com/docs/terminal/profiles#_git-bash
+export PROMPT_COMMAND="history -a;$PROMPT_COMMAND"
+
+# # https://docs.github.com/en/authentication/connecting-to-github-with-ssh/working-with-ssh-key-passphrases#auto-launching-ssh-agent-on-git-for-windows
+# env=~/.ssh/agent.env
+# agent_load_env () { test -f "$env" && . "$env" >| /dev/null ; }
+# agent_start () {
+#     (umask 077; ssh-agent >| "$env")
+#     . "$env" >| /dev/null ; }
+# agent_load_env
+# agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
+# if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
+#     agent_start
+#     ssh-add
+# elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
+#     ssh-add
+# fi
+# unset env
+
+# # Write the following line at the bottom of .bashrc
+# [[ ! ${BLE_VERSION-} ]] || ble-attach
