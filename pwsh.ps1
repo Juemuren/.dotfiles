58ac1d6f68552088193b1a3b108616a8920b3ff0
Raind <127611616+Juemuren@users.noreply.github.com>
2026-01-05 01:43:31 +0800

    pwsh: add dotfiles

---
+++ pwsh.ps1
@@ -0,0 +1,10 @@
+Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+Import-Module PSCompletions
+starship init powershell | Out-String | Invoke-Expression
+zoxide init powershell | Out-String | Invoke-Expression
+mise activate pwsh | Out-String | Invoke-Expression
+$env:PYTHONIOENCODING="utf-8"
+thefuck --alias | Out-String | Invoke-Expression
+# fastfetch
