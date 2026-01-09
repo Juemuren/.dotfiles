314632207022ecbc8020c5de2d6f52143ef497cc
Raind <127611616+Juemuren@users.noreply.github.com>
2026-01-10 00:31:12 +0800

    pwsh: add some scripts

---
+++ pwsh/Get-CommandContent.ps1
@@ -0,0 +1,12 @@
+param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$CommandName
+)
+
+$cmd = Get-Command $CommandName
+if (-not $cmd.Source) {
+    Write-Host "Command has no source file" -ForegroundColor Red
+    exit 1
+}
+
+Get-Content $cmd.Source
---
+++ pwsh/Get-HistoryFile.ps1
@@ -0,0 +1,1 @@
+Get-Content "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\$($Host.Name)_history.txt"
