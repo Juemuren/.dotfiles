5b457b83f486c82a9dc5e36f5759e8eff3654b2d
Raind <127611616+Juemuren@users.noreply.github.com>
2026-01-25 19:13:28 +0800

    bash: clean config for windows

--- bash/windows.bash
+++ bash/windows.bash
@@ -1,24 +1,8 @@
-# No interactively
+# Interactively
 [[ "$-" != *i* ]] && return
 
-# https://code.visualstudio.com/docs/terminal/profiles#_git-bash
+# History
 export PROMPT_COMMAND="history -a;$PROMPT_COMMAND"
-
-# https://docs.github.com/en/authentication/connecting-to-github-with-ssh/working-with-ssh-key-passphrases#auto-launching-ssh-agent-on-git-for-windows
-# env=~/.ssh/agent.env
-# agent_load_env () { test -f "$env" && . "$env" >| /dev/null ; }
-# agent_start () {
-#     (umask 077; ssh-agent >| "$env")
-#     . "$env" >| /dev/null ; }
-# agent_load_env
-# agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
-# if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
-#     agent_start
-#     ssh-add
-# elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
-#     ssh-add
-# fi
-# unset env
 
 # Integration
 # export PYTHONIOENCODING="utf-8"
