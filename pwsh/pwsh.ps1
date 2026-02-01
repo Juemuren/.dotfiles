0637787f18648c1e809ff5ba0b0042e681692829
Raind <127611616+Juemuren@users.noreply.github.com>
2026-02-01 22:26:41 +0800

    pwsh: fix config

--- pwsh/pwsh.ps1
+++ pwsh/pwsh.ps1
@@ -2,9 +2,7 @@ Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 Import-Module PSCompletions
-# $env:PYTHONIOENCODING="utf-8"
-# thefuck --alias | Out-String | Invoke-Expression
 mise activate pwsh | Out-String | Invoke-Expression
-zoxide init powershell | Out-String | Invoke-Expression
 starship init powershell | Out-String | Invoke-Expression
+zoxide init powershell | Out-String | Invoke-Expression
 # fastfetch
