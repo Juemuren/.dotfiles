31c00665eb095e235c828af1a6c77ad6164c5202
Raind <127611616+Juemuren@users.noreply.github.com>
2026-02-02 16:02:48 +0800

    pwsh: fix config

--- pwsh/pwsh.ps1
+++ pwsh/pwsh.ps1
@@ -2,7 +2,7 @@ Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 Import-Module PSCompletions
-mise activate pwsh | Out-String | Invoke-Expression
-starship init powershell | Out-String | Invoke-Expression
-zoxide init powershell | Out-String | Invoke-Expression
+(&mise activate pwsh) | Out-String | Invoke-Expression
+Invoke-Expression (&starship init powershell)
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 # fastfetch
