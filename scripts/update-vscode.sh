d7c854800cf5059d9a0da11f990664d767aca1ff
Raind <127611616+Juemuren@users.noreply.github.com>
2026-05-25 20:43:39 +0800

    vscode: sync settings

--- scripts/update-vscode.sh
+++ scripts/update-vscode.sh
@@ -7,10 +7,18 @@ extract_extensions() {
     jq -f "$script" "vscode/$profile/$profile.code-profile" > "vscode/$profile/extensions.jsonc"
 }
 
+extract_settings() {
+    profile=$1
+    script=$2
+
+    jq -r -f "$script" "vscode/$profile/$profile.code-profile" > "vscode/$profile/settings.jsonc"
+}
+
 profile=$1
 
 if [ "$profile" = "default" ]; then
     extract_extensions "$profile" scripts/extract-extensions-global.jq
 else
     extract_extensions "$profile" scripts/extract-extensions.jq
+    extract_settings "$profile" scripts/extract-settings.jq
 fi
