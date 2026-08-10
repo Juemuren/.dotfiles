80ad4358ce03d9163599620458f8de37cc4f22b4
Raind <127611616+Juemuren@users.noreply.github.com>
2026-08-11 03:36:01 +0800

    pwsh: suppress some lint output

--- scoop/shims/msedge.ps1
+++ scoop/shims/msedge.ps1
@@ -1,1 +1,8 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidUsingPositionalParameters',
+    '',
+    Justification = 'scoop uses positional CLI subcommands and arguments.'
+)]
+param()
+
 scoop shim add msedge 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
--- scoop/shims/vs.ps1
+++ scoop/shims/vs.ps1
@@ -1,1 +1,8 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidUsingPositionalParameters',
+    '',
+    Justification = 'scoop uses positional CLI subcommands and arguments.'
+)]
+param()
+
 scoop shim add vs '{{vs_root}}\Common7\Tools\Launch-VsDevShell.ps1' '--' -Arch amd64 -HostArch amd64 -SkipAutomaticLocation
