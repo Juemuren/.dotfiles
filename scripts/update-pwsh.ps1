cd895c1f9bb25629f45efe157b21b714a3785a49
Raind <127611616+Juemuren@users.noreply.github.com>
2026-08-10 08:31:03 +0800

    pwsh: add psc completions list

--- scripts/update-pwsh.ps1
+++ scripts/update-pwsh.ps1
@@ -1,2 +1,6 @@
 Get-InstalledModule |
 Select-Object -ExpandProperty Name > "pwsh/modules.txt"
+
+if (Get-Module -Name PSCompletions) {
+    psc list | Select-Object -ExpandProperty Completion > "pwsh/psc.txt"
+}
