644aa9e69acb47725923d3e326c8c85aa8340a18
Raind <127611616+Juemuren@users.noreply.github.com>
2026-08-12 07:20:47 +0800

    git: 使用 code 查看 git diff

--- bash/scripts/igit-show.sh
+++ bash/scripts/igit-show.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 GIT_SHOW="git show --color=always {1}"
+GIT_DIFFTOOL='git difftool "{r1}^!"'
 
 git log --oneline --decorate --color=always \
     | fzf --ansi \
         --preview "$GIT_SHOW" \
-        --bind "enter:become:$GIT_SHOW"
+        --header 'Enter: diff tool | Ctrl-P: print hash' \
+        --bind "enter:become:$GIT_DIFFTOOL" \
+        --bind "ctrl-p:become:echo {r1}"
