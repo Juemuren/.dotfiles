90e6c166b96f7e158b9da4b25fb5675c5473486e
Raind <127611616+Juemuren@users.noreply.github.com>
2026-08-13 06:59:02 +0800

    scoop: 使用 sfsu 替代 scoop 的部分命令

--- scripts/update-scoop.ps1
+++ scripts/update-scoop.ps1
@@ -1,4 +1,11 @@
-.\scoop\scripts\Get-ScoopBuckets.ps1
+# .\scoop\scripts\Get-ScoopBuckets.ps1
+# | ForEach-Object {
+#     .\scoop\scripts\Get-ScoopApps.ps1 "$_" > "scoop\buckets\$_.txt"
+# }
+
+sfsu bucket list --json
+| jq -r '.[].name'
 | ForEach-Object {
-    .\scoop\scripts\Get-ScoopApps.ps1 "$_" > "scoop\buckets\$_.txt"
+    sfsu list --json --descending --bucket $_
+    | jq -r '.[].name' > "scoop/buckets/$_.txt"
 }
