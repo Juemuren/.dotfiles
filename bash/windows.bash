a1b53b311d3681174f73edb8933011a1dedb9ba0
Raind <127611616+Juemuren@users.noreply.github.com>
2026-08-13 06:38:39 +0800

    mise: 修复 bash 中的路径问题

--- bash/windows.bash
+++ bash/windows.bash
@@ -5,6 +5,18 @@
 export PROMPT_COMMAND="history -a"
 
 # Integration
+# https://github.com/jdx/mise/discussions/3961
+eval "$(
+    mise activate bash \
+        | awk '
+            { print }
+
+            index($0, "eval \"$(mise hook-env ") ||
+            index($0, "eval \"$(command \"$__MISE_EXE\" ") {
+                print "export PATH=\"$(/usr/bin/cygpath -u -p \"$PATH\")\""
+            }
+        '
+)"
 eval "$(fzf --bash)"
 eval "$(zoxide init bash)"
 eval "$(starship init bash)"
