a6a0d529057e74149b4bd554c2837146472eabb3
Raind <127611616+Juemuren@users.noreply.github.com>
2026-08-14 11:12:46 +0800

    mise: 取消在 bash 中的激活

--- bash/windows.bash
+++ bash/windows.bash
@@ -6,17 +6,17 @@ export PROMPT_COMMAND="history -a"
 
 # Integration
 # https://github.com/jdx/mise/discussions/3961
-eval "$(
-    mise activate bash \
-        | awk '
-            { print }
+# eval "$(
+#     mise activate bash \
+#         | awk '
+#             { print }
 
-            index($0, "eval \"$(mise hook-env ") ||
-            index($0, "eval \"$(command \"$__MISE_EXE\" ") {
-                print "export PATH=\"$(/usr/bin/cygpath -u -p \"$PATH\")\""
-            }
-        '
-)"
+#             index($0, "eval \"$(mise hook-env ") ||
+#             index($0, "eval \"$(command \"$__MISE_EXE\" ") {
+#                 print "export PATH=\"$(/usr/bin/cygpath -u -p \"$PATH\")\""
+#             }
+#         '
+# )"
 eval "$(fzf --bash)"
 eval "$(zoxide init bash)"
 eval "$(starship init bash)"
