34c962e72b041b2392b6c10bee1df7820305b8d5
Raind <127611616+Juemuren@users.noreply.github.com>
2026-09-08 02:09:57 +0800

    env: Windows 添加 SCOOP_HOME 环境变量

--- env/windows.ps1
+++ env/windows.ps1
@@ -3,3 +3,10 @@
     "$env:USERPROFILE",
     'User'
 )
+
+$SCOOP_HOME=(Resolve-Path "$(scoop prefix scoop)\..\..\..").Path
+[System.Environment]::SetEnvironmentVariable(
+    'SCOOP_HOME',
+    "$SCOOP_HOME",
+    'User'
+)
