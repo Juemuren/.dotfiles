d0ab074849c66d09b659af1e27d8a6af535bb4cd
Raind <127611616+Juemuren@users.noreply.github.com>
2026-09-10 01:48:22 +0800

    vscode: 移除提取 settings.json 的脚本
    目前使用符号链接的方案

--- scripts/vscode/extract-settings.jq
+++
@@ -1,3 +0,0 @@
-.settings |
-fromjson |
-.settings
--- scripts/vscode/record.sh
+++ scripts/vscode/record.sh
@@ -12,19 +12,10 @@ extract_extensions() {
     jq -f "$script" "$PROFILES_DIR/$profile/$profile.code-profile" > "$PROFILES_DIR/$profile/extensions.jsonc"
 }
 
-extract_settings() {
-    profile=$1
-    script=$2
-
-    jq -r -f "$script" "$PROFILES_DIR/$profile/$profile.code-profile" > "$PROFILES_DIR/$profile/settings.jsonc"
-    dprint fmt "$PROFILES_DIR/$profile/settings.jsonc"
-}
-
 profile=$2
 
 if [ "$profile" = "global" ]; then
     extract_extensions "$profile" "$SCRIPT_DIR/extract-extensions-global.jq"
 else
     extract_extensions "$profile" "$SCRIPT_DIR/extract-extensions.jq"
-    # extract_settings "$profile" "$SCRIPT_DIR/extract-settings.jq"
 fi
