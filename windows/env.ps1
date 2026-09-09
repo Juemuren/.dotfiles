a2354bd2f0b4bdef179057257e02d5b37d574044
Raind <127611616+Juemuren@users.noreply.github.com>
2026-09-09 11:39:01 +0800

    windows: 修复 scoop 环境变量

--- windows/env.ps1
+++ windows/env.ps1
@@ -4,9 +4,16 @@
     'User'
 )
 
-$SCOOP_HOME = (Resolve-Path "$(scoop prefix scoop)\..\..\..").Path
+$SCOOP_HOME = Resolve-Path $(scoop prefix scoop)
 [System.Environment]::SetEnvironmentVariable(
     'SCOOP_HOME',
     "$SCOOP_HOME",
+    'User'
+)
+
+$SCOOP_ROOT = Resolve-Path "$(scoop prefix scoop)\..\..\.."
+[System.Environment]::SetEnvironmentVariable(
+    'SCOOP_ROOT',
+    "$SCOOP_ROOT",
     'User'
 )
