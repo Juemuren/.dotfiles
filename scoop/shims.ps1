5c8ff9449730948e76847477e5296456cd6e37e0
Raind <127611616+Juemuren@users.noreply.github.com>
2026-09-09 11:46:25 +0800

    scoop: 改善创建 shims 的脚本

--- scoop/shims.ps1
+++ scoop/shims.ps1
@@ -6,3 +6,4 @@
 param()
 
 scoop shim add msedge 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
+scoop shim add vs "$env:VS_ROOT\Common7\Tools\Launch-VsDevShell.ps1" '--' -Arch amd64 -HostArch amd64 -SkipAutomaticLocation
--- scoop/shims/vs.ps1
+++
@@ -1,8 +0,0 @@
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
-    'PSAvoidUsingPositionalParameters',
-    '',
-    Justification = 'scoop uses positional CLI subcommands and arguments.'
-)]
-param()
-
-scoop shim add vs '{{vs_root}}\Common7\Tools\Launch-VsDevShell.ps1' '--' -Arch amd64 -HostArch amd64 -SkipAutomaticLocation
