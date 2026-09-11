e41ad9c3e84f506f62ca9c33b1647d3a862189f0
Raind <127611616+Juemuren@users.noreply.github.com>
2026-09-11 19:45:53 +0800

    neovim: 拆分配置

--- neovim/vscode-neovim/init.lua
+++ neovim/vscode-neovim/init.lua
@@ -1,33 +1,8 @@
-vim.opt.clipboard = "unnamedplus"
-
 if not vim.g.vscode then
   return
 end
 
 vim.g.mapleader = " "
 
-local vscode = require("vscode")
-local map = vim.keymap.set
-
-local function action(name)
-  return function()
-    vscode.action(name)
-  end
-end
-
--- VS Code UI
-map("n", "<leader>p", action("workbench.action.quickOpen"),
-  { desc = "Quick Open" })
-
-map("n", "<leader>/", action("workbench.action.findInFiles"),
-  { desc = "Find in Files" })
-
-map("n", "<leader>e", action("workbench.view.explorer"),
-  { desc = "Explorer" })
-
--- Code actions
-map("n", "<leader>r", action("editor.action.rename"),
-  { desc = "Rename Symbol" })
-
-map("n", "<leader>a", action("editor.action.quickFix"),
-  { desc = "Code Action" })
+require("config.options")
+require("config.keymaps")
---
+++ neovim/vscode-neovim/lua/config/keymaps.lua
@@ -0,0 +1,26 @@
+local map = vim.keymap.set
+
+local vscode = require("vscode")
+
+local function action(name)
+  return function()
+    vscode.action(name)
+  end
+end
+
+-- VS Code UI
+map("n", "<leader>p", action("workbench.action.quickOpen"),
+  { desc = "Quick Open" })
+
+map("n", "<leader>/", action("workbench.action.findInFiles"),
+  { desc = "Find in Files" })
+
+map("n", "<leader>e", action("workbench.view.explorer"),
+  { desc = "Explorer" })
+
+-- Code actions
+map("n", "<leader>r", action("editor.action.rename"),
+  { desc = "Rename Symbol" })
+
+map("n", "<leader>a", action("editor.action.quickFix"),
+  { desc = "Code Action" })
