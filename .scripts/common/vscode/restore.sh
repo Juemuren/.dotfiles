8a302eda8b542b6399f054e1b90ed3ff765a21ae
Raind <127611616+Juemuren@users.noreply.github.com>
2026-09-19 10:18:47 +0800

    vscode: 恢复脚本更新
    - 可以安装 global 扩展，但仍需手动应用到全部配置文件
    - 可以一次性恢复所有 profile

--- .scripts/common/vscode/restore.sh
+++ .scripts/common/vscode/restore.sh
@@ -3,8 +3,9 @@
 set -eu
 
 PROFILES_DIR=$1
+GLOBAL_PROFILE_NAME="global"
 
-install_extensions() {
+install_profile_extensions() {
     local profile=$1
 
     while IFS= read -r extension; do
@@ -12,11 +13,33 @@ install_extensions() {
     done < "$PROFILES_DIR/$profile/extensions.txt"
 }
 
-profile=$2
+install_global_extensions() {
+    # code 目前不支持在安装扩展时应用到全部配置文件，需要后续手动设置
+    while IFS= read -r extension; do
+        code --install-extension "$extension"
+    done < "$PROFILES_DIR/$GLOBAL_PROFILE_NAME/extensions.txt"
+}
 
-if [ "$profile" = "global" ]; then
-    # code 目前不支持安装扩展时应用到全部配置文件
-    exit 1
-else
-    install_extensions "$profile"
-fi
+install_all_extensions() {
+    install_global_extensions
+    for profile_dir in "$PROFILES_DIR"/*/; do
+        profile_name=$(basename "$profile_dir")
+        if [ "$profile_name" != "$GLOBAL_PROFILE_NAME" ]; then
+            install_profile_extensions "$profile_name"
+        fi
+    done
+}
+
+profile_name=$2
+
+case "$profile_name" in
+    "$GLOBAL_PROFILE_NAME")
+        install_global_extensions
+        ;;
+    all)
+        install_all_extensions
+        ;;
+    *)
+        install_profile_extensions "$profile_name"
+        ;;
+esac
